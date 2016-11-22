70bb42bbe9813dc4becb2a0a9f07198ce847a24f
Tarek Almouhtasseb <tarek@parasailhealth.com>
2016-11-22 11:10:52 -0800

    added more files

---
+++ some_folder/some_file.py
@@ -0,0 +1,13 @@
+def info():
+    return "this file is a test file"
+
+def nonsense(a,b):
+    return b - a + a - b
+
+def some_func_that_does_nothing(a,b,c):
+    return 
+
+def do_all():
+    info()
+    nonsense(1,2)
+    some_func_that_does_nothing(3,2,1)
